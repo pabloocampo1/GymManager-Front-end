87000d7a029b7ccd5422666cf7a3f5e380b5e3d1
pablo ocampo <juanpabloocampoleon@gmail.com>
2025-03-13 08:57:47 -0500

    se agrego iconos a los detalles de contacto

--- src/Pages/SectionHomePages/ContactHome/ContactHome.jsx
+++ src/Pages/SectionHomePages/ContactHome/ContactHome.jsx
@@ -1,28 +1,70 @@
 import React from 'react';
 import style from "./ContactHome.module.css"
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const ContactHome = () => {
     return (
         <div className={style.ContactHome_container}>
-
-            {/* intro de la seccion de contacto */}
             <div className={style.container_intro_contact}>
                 defd
             </div>
-            {/*seccion de contacto */}
+            
             <div className={style.container_contact}>
                 <div className={style.section_details}>
                     <div className={style.section_details_information}>
                         <h2>Nuestros detalles de contacto</h2>
                         <div className={style.information_items}>
-                            <div>1</div>
-                            <div>2</div>
-                            <div>3</div>
-                            <div>4</div>
+                            <div>
+                                <div className={style.logo_details}>
+                                    <LocalPhoneIcon />
+                                </div>
+                                <div className={style.details_about_contact} >
+                                    <h3>Tefonos decontacto</h3>
+                                    <ol>
+                                        <li>343434</li>
+                                        <li>34343</li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div><div className={style.logo_details}>
+                                <EmailIcon> </EmailIcon>
+                            </div>
+                                <div className={style.details_about_contact} >
+                                    <h3>Tefonos decontacto</h3>
+                                    <ol>
+                                        <li>343434</li>
+                                        <li>34343</li>
+                                    </ol>
+                                </div></div>
+                            <div>
+                                <div className={style.logo_details}>
+                                    <LocationOnIcon />
+                                </div>
+                                <div className={style.details_about_contact} >
+                                    <h3>Tefonos decontacto</h3>
+                                    <ol>
+                                        <li>343434</li>
+                                        <li>34343</li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div><div className={style.logo_details}>
+                                <AccessTimeFilledIcon />
+                            </div>
+                                <div className={style.details_about_contact} >
+                                    <h3>Tefonos decontacto</h3>
+                                    <ol>
+                                        <li>343434</li>
+                                        <li>34343</li>
+                                    </ol>
+                                </div></div>
                         </div>
                     </div>
                     <div className={style.section_details_map}>
-                            fdfdfd
+                        fdfdfd
                     </div>
                 </div>
                 <div className={style.section_send_mesagge}>
@@ -30,10 +72,10 @@ const ContactHome = () => {
                         <h2>Envianos un mensaje</h2>
                         <div className={style.section_send_mesagge_items}>
                             <div className={style.send_mesagge_form_container}>
-d
+                                d
                             </div>
                             <div className={style.send_mesagge_socialMedia_container}>
-f
+                                f
                             </div>
                         </div>
                     </div>
