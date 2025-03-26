abc2d5729462afaf986c0998f0166cef5146a3d6
Mateo Asus <m2a3c11@gmail.com>
2025-03-26 08:31:24 -0500

    Actualizacion membresias

--- src/Pages/Membresias/Membresias.jsx
+++ src/Pages/Membresias/Membresias.jsx
@@ -6,7 +6,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import React, { useState } from "react";
 import styles from "./Membresias.module.css";
 import MembresiasModal from "../../Components/Modals/ModalMembresias/MembresiasModal.jsx";
@@ -17,10 +17,34 @@ const MembresiaModal = () => {
   const [selectedFilter, setSelectedFilter] = useState("Todos");
 
   const membresias = [
-    { id: 1, name: "Membresía Oro", duracion: "30 Días", precio: "$70,000", type: "Oro" },
-    { id: 2, name: "Membresía Plata", duracion: "30 Días", precio: "$50,000", type: "Plata" },
-    { id: 3, name: "Membresía Bronce", duracion: "15 Días", precio: "$30,000", type: "Bronce" },
-    { id: 4, name: "Membresía Oro Plus", duracion: "45 Días", precio: "$90,000", type: "Oro" },
+    {
+      id: 1,
+      name: "Membresía Oro",
+      duracion: "30 Días",
+      precio: "$70,000",
+      type: "Oro",
+    },
+    {
+      id: 2,
+      name: "Membresía Plata",
+      duracion: "30 Días",
+      precio: "$50,000",
+      type: "Plata",
+    },
+    {
+      id: 3,
+      name: "Membresía Bronce",
+      duracion: "15 Días",
+      precio: "$30,000",
+      type: "Bronce",
+    },
+    {
+      id: 4,
+      name: "Membresía Oro Plus",
+      duracion: "45 Días",
+      precio: "$90,000",
+      type: "Oro",
+    },
   ];
 
   const handleOpenMenu = (event) => {
@@ -45,57 +69,70 @@ const MembresiaModal = () => {
         <h2 className={styles.membresias_title}>Membresías</h2>
         <div className={styles.search_container}>
           <FaSearch className={styles.search_icon} />
-          <input type="text" placeholder="Buscar membresías" className={styles.search_input} />
+          <input
+            type="text"
+            placeholder="Buscar membresías"
+            className={styles.search_input}
+          />
         </div>
-        
+
         <button className={styles.filter_boton} onClick={handleOpenMenu}>
           <FaFilter className={styles.filter_icon} /> Filtrar
         </button>
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleCloseMenu()}>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={() => handleCloseMenu()}
+        >
           <MenuItem onClick={() => handleCloseMenu("Oro")}>Oro</MenuItem>
-          <MenuItem onClick={() => handleCloseMenu("Plata")} >Plata</MenuItem>
-          <MenuItem onClick={() => handleCloseMenu("Bronce")} >Bronce</MenuItem>
-          <MenuItem onClick={() => handleCloseMenu("Todos")} >Todos</MenuItem>
+          <MenuItem onClick={() => handleCloseMenu("Plata")}>Plata</MenuItem>
+          <MenuItem onClick={() => handleCloseMenu("Bronce")}>Bronce</MenuItem>
+          <MenuItem onClick={() => handleCloseMenu("Todos")}>Todos</MenuItem>
         </Menu>
-        
-        <button className={styles.add_boton} onClick={() => setIsModalOpen(true)}>
+
+        <button
+          className={styles.add_boton}
+          onClick={() => setIsModalOpen(true)}
+        >
           <FaSearch className={styles.add_icon} /> Agregar Nueva Membresía
         </button>
       </div>
-      
-      <h2 className={styles.filtered_title}>Filtrado por la membresia: {selectedFilter}</h2>
-      <TableContainer className={styles.membresia_table}/>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Duración</TableCell>
-              <TableCell>Precio</TableCell>
-              <TableCell>Tipo</TableCell>
-              <TableCell>Opciones</TableCell>
+
+      <h2 className={styles.filtered_title}>
+        Filtrado por la membresia: {selectedFilter}
+      </h2>
+      <TableContainer className={styles.membresia_table} />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Duración</TableCell>
+            <TableCell>Precio</TableCell>
+            <TableCell>Tipo</TableCell>
+            <TableCell>Opciones</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredMembresias.map((membresia) => (
+            <TableRow key={membresia.id} className={styles.membresia_row}>
+              <TableCell>{membresia.id}</TableCell>
+              <TableCell>{membresia.name}</TableCell>
+              <TableCell>{membresia.duracion}</TableCell>
+              <TableCell>{membresia.precio}</TableCell>
+              <TableCell>{membresia.type}</TableCell>
+              <TableCell>
+                <FaPen
+                  className={styles.edit_icon}
+                  onClick={() => setIsModalOpen(true)}
+                />
+                <DeleteIcon className={styles.delete_icon} />
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredMembresias.map((membresia) => (
-              <TableRow key={membresia.id} className={styles.membresia_row}>
-                <TableCell>{membresia.id}</TableCell>
-                <TableCell>{membresia.name}</TableCell>
-                <TableCell>{membresia.duracion}</TableCell>
-                <TableCell>{membresia.precio}</TableCell>
-                <TableCell>{membresia.type}</TableCell>
-                <TableCell>
-                  <FaPen
-                    className={styles.edit_icon}
-                    onClick={() => setIsModalOpen(true)}
-                  />
-                  <DeleteIcon className={styles.delete_icon} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+          ))}
+        </TableBody>
       </Table>
-      
+
       {isModalOpen && (
         <MembresiasModal
           isOpen={isModalOpen}
--- src/Pages/Miembros/Miembros.jsx
+++ src/Pages/Miembros/Miembros.jsx
@@ -6,7 +6,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import React, { useState } from "react";
 import styles from "./Miembros.module.css";
 import MiembrosModal from "../../Components/Modals/ModalMiembros/MiembrosModal.jsx";
@@ -17,9 +17,33 @@ const MiembrosModalComponent = () => {
   const [selectedFilter, setSelectedFilter] = useState("Todos");
 
   const miembros = [
-    { id: 1, name: "Mateo Arias Carbon", telefono: "3232323763", estado: "Activo", tipo: "Mensual", inicio: "12 - 05 - 2024", fin: "12 - 06 - 2024" },
-    { id: 2, name: "Mateo Arias Carbon", telefono: "3232323763", estado: "Activo", tipo: "Mensual", inicio: "12 - 05 - 2024", fin: "12 - 06 - 2024" },
-    { id: 3, name: "Mateo Arias Carbon", telefono: "3232323763", estado: "Activo", tipo: "Mensual", inicio: "12 - 05 - 2024", fin: "12 - 06 - 2024" },
+    {
+      id: 1,
+      name: "Mateo Arias Carbon",
+      telefono: "3232323763",
+      estado: "Activo",
+      tipo: "Mensual",
+      inicio: "12 - 05 - 2024",
+      fin: "12 - 06 - 2024",
+    },
+    {
+      id: 2,
+      name: "Mateo Arias Carbon",
+      telefono: "3232323763",
+      estado: "Activo",
+      tipo: "Mensual",
+      inicio: "12 - 05 - 2024",
+      fin: "12 - 06 - 2024",
+    },
+    {
+      id: 3,
+      name: "Mateo Arias Carbon",
+      telefono: "3232323763",
+      estado: "Activo",
+      tipo: "Mensual",
+      inicio: "12 - 05 - 2024",
+      fin: "12 - 06 - 2024",
+    },
   ];
 
   const handleOpenMenu = (event) => {
@@ -44,59 +68,83 @@ const MiembrosModalComponent = () => {
         <h2 className={styles.miembros_title}>Miembros</h2>
         <div className={styles.search_container}>
           <FaSearch className={styles.search_icon} />
-          <input type="text" placeholder="Buscar miembros" className={styles.search_input} />
+          <input
+            type="text"
+            placeholder="Buscar miembros"
+            className={styles.search_input}
+          />
         </div>
-        
+
         <button className={styles.filter_boton} onClick={handleOpenMenu}>
           <FaFilter className={styles.filter_icon} /> Filtrar
         </button>
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleCloseMenu()}>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={() => handleCloseMenu()}
+        >
           <MenuItem onClick={() => handleCloseMenu("Activo")}>Activo</MenuItem>
-          <MenuItem onClick={() => handleCloseMenu("Inactivo")}>Inactivo</MenuItem>
+          <MenuItem onClick={() => handleCloseMenu("Inactivo")}>
+            Inactivo
+          </MenuItem>
           <MenuItem onClick={() => handleCloseMenu("Todos")}>Todos</MenuItem>
         </Menu>
-        
-        <button className={styles.add_boton} onClick={() => setIsModalOpen(true)}>
+
+        <button
+          className={styles.add_boton}
+          onClick={() => setIsModalOpen(true)}
+        >
           <FaSearch className={styles.add_icon} /> Agregar Nuevo Miembro
         </button>
       </div>
-      
-      <h2 className={styles.filtered_title}>Filtrado por estado: {selectedFilter}</h2>
-      <TableContainer className={styles.miembros_table}/>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Documento</TableCell>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Teléfono</TableCell>
-              <TableCell>Estado Membresía</TableCell>
-              <TableCell>Tipo de Membresía</TableCell>
-              <TableCell>Inicio Membresía</TableCell>
-              <TableCell>Fin Membresía</TableCell>
-              <TableCell>Opciones</TableCell>
+
+      <h2 className={styles.filtered_title}>
+        Filtrado por estado: {selectedFilter}
+      </h2>
+      <TableContainer className={styles.miembros_table} />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Documento</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Teléfono</TableCell>
+            <TableCell>Estado Membresía</TableCell>
+            <TableCell>Tipo de Membresía</TableCell>
+            <TableCell>Inicio Membresía</TableCell>
+            <TableCell>Fin Membresía</TableCell>
+            <TableCell>Opciones</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredMiembros.map((miembro) => (
+            <TableRow key={miembro.id} className={styles.miembros_row}>
+              <TableCell>{miembro.id}</TableCell>
+              <TableCell>{miembro.name}</TableCell>
+              <TableCell>{miembro.telefono}</TableCell>
+              <TableCell
+                className={
+                  miembro.estado === "Activo"
+                    ? styles.estado_activo
+                    : styles.estado_inactivo
+                }
+              >
+                {miembro.estado}
+              </TableCell>
+              <TableCell>{miembro.tipo}</TableCell>
+              <TableCell>{miembro.inicio}</TableCell>
+              <TableCell>{miembro.fin}</TableCell>
+              <TableCell>
+                <FaPen
+                  className={styles.edit_icon}
+                  onClick={() => setIsModalOpen(true)}
+                />
+                <DeleteIcon className={styles.delete_icon} />
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredMiembros.map((miembro) => (
-              <TableRow key={miembro.id} className={styles.miembros_row}>
-                <TableCell>{miembro.id}</TableCell>
-                <TableCell>{miembro.name}</TableCell>
-                <TableCell>{miembro.telefono}</TableCell>
-                <TableCell className={miembro.estado === "Activo" ? styles.estado_activo : styles.estado_inactivo}>
-                  {miembro.estado}
-                </TableCell>
-                <TableCell>{miembro.tipo}</TableCell>
-                <TableCell>{miembro.inicio}</TableCell>
-                <TableCell>{miembro.fin}</TableCell>
-                <TableCell>
-                  <FaPen className={styles.edit_icon} onClick={() => setIsModalOpen(true)} />
-                  <DeleteIcon className={styles.delete_icon} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+          ))}
+        </TableBody>
       </Table>
-      
+
       {isModalOpen && (
         <MiembrosModal
           isOpen={isModalOpen}
