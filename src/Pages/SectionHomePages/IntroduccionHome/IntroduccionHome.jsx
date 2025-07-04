a6498cbc38ad81e894e6e1cc10162fe5356d1e5a
pablo ocampo <juanpabloocampoleon@gmail.com>
2025-07-04 14:17:53 -0500

    Style(home):change the UI of the introduction of the home

--- src/Pages/SectionHomePages/IntroduccionHome/IntroduccionHome.jsx
+++ src/Pages/SectionHomePages/IntroduccionHome/IntroduccionHome.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Button, styled, keyframes, useTheme } from '@mui/material';
+import { Box, Typography, Button, styled, keyframes } from '@mui/material';
 import { motion } from 'framer-motion';
 import { Link as RouterLink } from 'react-router-dom';
 
-import imagen1 from '../../../assets/images/imagenValHallaSlider1.png';
+
 import imagen2 from '../../../assets/images/imagenValHallaSlider2.png';
 import imagen3 from '../../../assets/images/imagenValHallaSlider3.png';
 
@@ -27,12 +27,11 @@ const Cursor = styled('span')(({ theme }) => ({
     animation: `${blink} 0.7s steps(1) infinite`,
 }));
 
-const sliderDuration = 10;
+
 
 export default function IntroduccionHome() {
-    const theme = useTheme();
-    const images = [imagen1, imagen2, imagen3];
-    const fullText = 'VALLHALLA\nGYM';
+  
+    const fullText = 'VALLHALLA\nGYM.';
     const [typedText, setTypedText] = useState('');
 
     useEffect(() => {
@@ -46,121 +45,145 @@ export default function IntroduccionHome() {
         return () => clearInterval(interval);
     }, []);
 
-    return (
-        <Box
-            id="home"
-            sx={{
-                width: '100%',
-                height: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                backgroundColor: 'background.paper',
-                px: { xs: 2, md: 12 },
-                flexDirection: { xs: 'column', md: 'row' },
-                mb: { xs: 12, md: 0 },
-            }}
-        >
-            {/* Left Info */}
-            <Box
-                sx={{
-                    width: { xs: '80%', md: '50%' },
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}
-            >
-                <Typography
-                    variant="h6"
-                    sx={{
-                        fontFamily: 'Big Shoulders Stencil, sans-serif',
-                        fontSize: '1.3rem',
-                        pb: 0.5,
-                        color: "primary.main"
-                    }}
-                >
-                El salon de los dioses
-                </Typography>
-                <Typography
-                    component="h1"
-                    sx={{
-                        fontWeight: 'bold',
-                        fontSize: '3rem',
-                        pt: 1,
-                        pb: 0.5,
-                    }}
-                >
-                    <Typewriter>
-                        {typedText.split('\n').map((line, i) => (
-                            <React.Fragment key={i}>
-                                {line}
-                                {i < typedText.split('\n').length - 1 && <br />}
-                            </React.Fragment>
-                        ))}
-                        <Cursor />
-                    </Typewriter>
-                </Typography>
-                <Typography
-                    variant="body1"
-                    sx={{
-                        fontSize: '1rem',
-                        pt: 0.5,
-                        pb: 1,
-                    }}
-                >
-                    Transforma tu cuerpo, transforma tu <br /> vida. ¡Únete a nuestra comunidad <br /> fitness!
-                </Typography>
-                <Button
-                    component={RouterLink}
-                    to="#contact"
-                    variant="outlined"
-                    sx={{
-                        width: 120,
-                        height: 40,
-                        mt: 4,
-                        borderColor: "text.secondary",
-                        borderRadius: 2,
-                       
-                        color:"text.secondary",
-                        '&:hover': {
-                            borderWidth: 2,
-                            backgroundColor: 'rgba(242, 242, 241,)',
-                        },
-                    }}
-                >
-                    Contactanos
-                </Button>
-            </Box>
 
-            {/* Right Slider */}
+
+
+    return (
+        <>
+            
+        <Box
+            sx={{
+                position: 'absolute',
+                top: "0",
+                left: 0,
+                width: '100%',
+                height: '109vh',
+                bgcolor: 'rgba(0, 0, 0, 0.7)', 
+                zIndex: 1,
+            }}
+        />
+        
             <Box
+                id="home"
                 sx={{
-                    width: { xs: '80%', md: '50%' },
-                    height: '100%',
+                    position: 'relative', 
+                    width: '100%',
+                    height: '100vh',
                     display: 'flex',
                     alignItems: 'center',
-                    justifyContent: 'center',
-                    position: 'relative',
-                    overflow: 'hidden',
+                    justifyContent: 'space-between',
+                    backgroundImage: `url(${imagen2})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    px: { xs: 2, md: 12 },
+                    flexDirection: { xs: 'column', md: 'row' },
+                    mb: { xs: 12, md: 0 },
                 }}
             >
-                <motion.div
-                    style={{ display: 'flex', borderRadius: '50%', height: '70%', width: `calc(70% * 4)`, gap: '20px' }}
-                    animate={{ x: ['0%', '-100%'] }}
-                    transition={{ repeat: Infinity, duration: sliderDuration, ease: 'linear' }}
+               
+                <Box
+                    sx={{
+                         zIndex: 2,
+                        width: { xs: '80%', md: '50%' },
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: "start",
+                        pl: "30px"
+                    }}
                 >
-                    {images.concat(images).map((img, index) => (
-                        <Box
-                            component="img"
-                            key={index}
-                            src={img}
-                            alt={`slide-${index}`}
-                            sx={{ height: '100%', flexShrink: 0 }}
-                        />
-                    ))}
-                </motion.div>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            fontFamily: 'Big Shoulders Stencil, sans-serif',
+                            fontSize: '1.3rem',
+                            pb: 0,
+                            color: "primary.main"
+                        }}
+                    >
+                        El salon de los dioses¡
+                    </Typography>
+                    <Typography
+                        component="h1"
+                        sx={{
+                            fontWeight: 'bold',
+                            fontSize: '3rem',
+                            pt: 0,
+                            pb: 1,
+                        }}
+                    >
+                        <Typewriter>
+                            {typedText.split('\n').map((line, i) => (
+                                <React.Fragment key={i}>
+                                    {line}
+                                    {i < typedText.split('\n').length - 1 && <br />}
+                                </React.Fragment>
+                            ))}
+                            <Cursor />
+                        </Typewriter>
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        sx={{
+                            fontSize: '1rem',
+                            pt: 2,
+                            pb: 1,
+                        }}
+                    >
+                        Transforma tu cuerpo y fortalece tu mente. <br />
+                        Da el primer paso hacia una versión más fuerte y saludable de ti mismo. <br />
+                        ¡Únete a nuestra comunidad fitness y vive la experiencia VALLHALLA!
+                    </Typography>
+                    <Button
+                        component={RouterLink}
+                        to="contact#contact"
+                        variant="outlined"
+                        sx={{
+                            width: 120,
+                            height: 40,
+                            mt: 4,
+                            borderColor: "text.secondary",
+                            borderRadius: 2,
+
+                            color: "text.secondary",
+                            '&:hover': {
+                                borderWidth: 2,
+                                backgroundColor: 'rgba(242, 242, 241,)',
+                            },
+                        }}
+                    >
+                        Contactanos
+                    </Button>
+                </Box>
+
+                
+                <Box
+                    sx={{
+                         zIndex: 2,
+                        width: { xs: '80%', md: '50%' },
+                        height: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+
+                    }}
+                >
+                    <Box sx={{
+                        width: "80%", height: "80%", backgroundImage: `url(${imagen3})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        borderRadius: "30px"
+                    }}>
+
+                    </Box>
+                </Box>
             </Box>
-        </Box>
+        </>
+
+
     );
 }
+
